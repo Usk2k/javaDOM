bbff5277cf02c5d6be5171f3350ca1d8275a9d7c
Usk2k <usuhuu321321@gmail.com>
2022-12-21 12:44:50 +0800

    pass

---
+++ pass/pass.js
@@ -0,0 +1,7 @@
+let password = "11";
+let button = document.querySelector("button");
+let input = document.querySelector("input");
+button.addEventListener("click", () => {
+  if (input.value == password) {
+  }
+});
